1d5bf85e938edd3bfb47a75d2a81ff7f988247a7
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-05-20 14:59:15 +0530

    Heroku configs

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = express();
 
 app.use(bodyParser.json());
 var authenticationController = require('./server/controllers/authentication-controller');
-
-mongoose.connect('mongodb://localhost:27017/time-pass');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/time-pass');
+//mongoose.connect('mongodb://localhost:27017/time-pass');
 
 app.use('/app' , express.static( __dirname + "/app" ));
 app.use('/node_modules', express.static(__dirname + "/node_modules"));
@@ -17,10 +17,10 @@ app.get('/', function(req,res){
 	res.sendFile( __dirname + "/index.html");
 })
 
-
+port = process.env.PORT || 3000;
 //Authentication
 app.post('/api/user/signup', authenticationController.signup);
 app.post('/api/user/login', authenticationController.login);
 app.listen('3000', function(){
-	console.log("Listening localhost port 3000");
+	console.log("Listening on port" + port);
 });
